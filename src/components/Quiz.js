38d6207d18d085cfe206a16e2861ca30da4b2fcd
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-17 18:57:32 -0400

    Add option set in quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -6,11 +6,13 @@ import QuestionCounter from './QuestionCounter'
 import Category from './Category'
 import DifficultyRating from './DifficultyRating'
 import Question from './Question'
+import OptionSet from './OptionSet'
 import questions from '../questions.json'
 
 function Quiz () {
   const [ currentRound ] = useState(10)
   const q = questions[0]
+  const [ , setScore ] = useState(0)
 
   return <div className='quiz'>
     <ProgressBar maxQuestions={questions.length} currentQuestion={currentRound} />
@@ -18,6 +20,7 @@ function Quiz () {
     <Category text={q.category} />
     <DifficultyRating difficulty={q.difficulty} />
     <Question text={q.question} />
+    <OptionSet correctAnswer={q.correct_answer} incorrectAnswers={q.incorrect_answers} handleCorrect={setScore} handleIncorrect={setScore} />
   </div>
 }
 
