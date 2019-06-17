8508f2b147c5b4c0c7da6af4cace012f8c885b41
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-17 11:17:49 -0400

    implement difficulty rating

--- src/components/DifficultyRating.js
+++ src/components/DifficultyRating.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import DifficultyStar from './DifficultyStar'
 
 function DifficultyRating (props) {
-  return <div>{props.difficulty}</div>
+  return <div>
+    <DifficultyStar darken />
+    <DifficultyStar darken={props.difficulty !== 'easy'} />
+    <DifficultyStar darken={props.difficulty === 'hard'} />
+  </div>
 }
 
 export default DifficultyRating
