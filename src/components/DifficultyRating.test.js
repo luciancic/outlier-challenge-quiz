206b5057501fe5d65ac3d91adaecc1a46b104241
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-17 11:13:22 -0400

    write difficulty rating tests

--- src/components/DifficultyRating.test.js
+++ src/components/DifficultyRating.test.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import DifficultyRating from './DifficultyRating'
+import DifficultyStar from './DifficultyStar'
 
 it('should render 3 stars', function () {
-  const wrapper = shallow(<DifficultyRating difficulty='hard' />)
-  // expect find(DifficultyStar).length to be 3
+  const wrapper = shallow(<DifficultyRating difficulty='easy' />)
+  expect(wrapper.find(DifficultyStar).length).toBe(3)
 })
 
 it('should darken 1 star on easy', function () {
   const wrapper = shallow(<DifficultyRating difficulty='easy' />)
-  // const stars = wrapper.find(DifficultyStar)
-  // expect stars.find('.darken').length to be 1
+  const stars = wrapper.find(DifficultyStar)
+
+  expect(stars.find({ darken: true }).length).toBe(1)
 })
 
 it('should darken 2 stars on medium', function () {
   const wrapper = shallow(<DifficultyRating difficulty='medium' />)
-  // const stars = wrapper.find(DifficultyStar)
-  // expect stars.find('.darken').length to be 2
+  const stars = wrapper.find(DifficultyStar)
+
+  expect(stars.find({ darken: true }).length).toBe(2)
 })
 
 it('should darken 3 stars on hard', function () {
   const wrapper = shallow(<DifficultyRating difficulty='hard' />)
-  // const stars = wrapper.find(DifficultyStar)
-  // expect stars.find('.darken').length to be 3
+  const stars = wrapper.find(DifficultyStar)
+
+  expect(stars.find({ darken: true }).length).toBe(3)
 })
