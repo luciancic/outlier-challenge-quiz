cfd0b5475b530ab4c919a3d1c41f63b9e15fd2f8
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-17 00:22:51 -0400

    write question counter test

--- src/components/QuestionCounter.test.js
+++ src/components/QuestionCounter.test.js
@@ -3,5 +3,6 @@ import { shallow } from 'enzyme'
 import QuestionCounter from './QuestionCounter'
 
 it('should count questions', function () {
-
+  const wrapper = shallow(<QuestionCounter maxQuestions={10} currentQuestion={3} />)
+  expect(wrapper.text()).toEqual('Question 3 of 10')
 })
