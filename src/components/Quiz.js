74339dbadf7abe1fe78b9dfffdf5b8a856a2441e
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-17 11:29:08 -0400

    create Question component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,6 +5,7 @@ import ProgressBar from './ProgressBar'
 import QuestionCounter from './QuestionCounter'
 import Category from './Category'
 import DifficultyRating from './DifficultyRating'
+import Question from './Question'
 import questions from '../questions.json'
 
 function Quiz () {
@@ -15,6 +16,7 @@ function Quiz () {
     <QuestionCounter maxQuestions={questions.length} currentQuestion={currentRound} />
     <Category text={questions[0].category} />
     <DifficultyRating difficulty={questions[0].difficulty} />
+    <Question text={questions[0].question} />
   </div>
 }
 
