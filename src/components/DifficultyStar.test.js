f638e024e5b61a84bd146803a8724a6b3c978828
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-17 10:57:46 -0400

    write difficulty star tests

---
+++ src/components/DifficultyStar.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import DifficultyStar from './DifficultyStar'
+
+it('should darken correclty', function () {
+  const wrapper = shallow(<DifficultyStar darken={false} />)
+  expect(wrapper.find('.quiz__star--darken').length).toBe(0)
+
+  wrapper.setProps({ darken: true })
+  expect(wrapper.find('.quiz__star--darken').length).toBe(1)
+})
