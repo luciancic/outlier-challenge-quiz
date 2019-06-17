e2ae2f834cbd32271886cc8a0d380a4110cc85a1
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-17 10:26:53 -0400

    Add spacing inside body of quiz

--- src/components/QuestionCounter.js
+++ src/components/QuestionCounter.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function QuestionCounter (props) {
   const { maxQuestions, currentQuestion } = props
-  return <h1>Question {currentQuestion} of {maxQuestions}</h1>
+  return <h1 className='quiz__question-counter'>Question {currentQuestion} of {maxQuestions}</h1>
 }
 
 export default QuestionCounter
