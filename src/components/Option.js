f6bf3390044da04a779d2ba6ee603152cdc19c17
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-18 14:39:09 -0400

    add className to option based on passed style

--- src/components/Option.js
+++ src/components/Option.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Option (props) {
   const text = decodeURIComponent(props.text)
+  const stateStyle = props.style !== undefined ? 'quiz__option--' + props.style : ''
+  const className = 'quiz__option ' + stateStyle
 
-  return <button onClick={props.handler}>{text}</button>
+  return <button onClick={props.handler} className={className}>{text}</button>
 }
 
 export default Option
