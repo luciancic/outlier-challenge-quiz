7c2b63fe9d1c02547d138af87b6422e3dbb2d960
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-19 18:44:43 -0400

    move option styling tests to quiz

--- src/components/Quiz.test.js
+++ src/components/Quiz.test.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render, fireEvent } from '@testing-library/react'
 import Quiz from './Quiz'
 
-/* global it */
+/* global it, expect, beforeEach */
+
+let quiz
+
+beforeEach(function () {
+  quiz = render(<Quiz />)
+})
+
 it('renders without crashing', () => { // eslint-disable-line
-  const div = document.createElement('div')
-  ReactDOM.render(<Quiz />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  expect(quiz.baseElement).toBeInTheDOM()
+})
+
+it('should style chosen option', function () {
+  const dirk = quiz.getByText('Dirk the Daring')
+  expect(dirk).toHaveClass('quiz__option')
+  expect(dirk).not.toHaveClass('quiz__option--chosen')
+  fireEvent.click(dirk)
+  expect(dirk).toHaveClass('quiz__option--chosen')
+})
+
+it('should style disabled options', function () {
+  const dirk = quiz.getByText('Dirk the Daring')
+  const arthur = quiz.getByText('Arthur')
+  const toby = quiz.getByText('Sir Toby Belch')
+  const guy = quiz.getByText('Guy of Gisbourne')
+
+  fireEvent.click(guy)
+  expect(guy).toHaveClass('quiz__option--chosen')
+  expect(dirk).toHaveClass('quiz__option--missed')
+  expect(arthur).toHaveClass('quiz__option--disabled')
+  expect(toby).toHaveClass('quiz__option--disabled')
 })
