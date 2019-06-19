ebaf2400f9af3a085f5251d09035cf0bfbdeb7b0
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-19 18:24:16 -0400

    setup jest-dom and react testing lib

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,6 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import '@testing-library/react/cleanup-after-each'
+import 'jest-dom/extend-expect'
 
 Enzyme.configure({ adapter: new Adapter() })
