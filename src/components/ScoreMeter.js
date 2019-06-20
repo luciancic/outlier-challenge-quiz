9a043c6beac77dacbfa1a63613e1c08685be8919
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-19 22:49:12 -0400

    basic implementation of score meter

--- src/components/ScoreMeter.js
+++ src/components/ScoreMeter.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 function ScoreMeter (props) {
-  return <div>Your score!</div>
+  const { currentRound, maxRounds, mistakes } = props
+  const score = Math.floor((currentRound - mistakes) / currentRound * 100)
+  const minScore = Math.floor(mistakes / maxRounds * 100)
+  const maxScore = Math.floor((maxRounds - mistakes) / maxRounds * 100)
+
+  return <div>
+    <span>Score: {score}%</span>
+    <span>Max Score: {maxScore}%</span>
+    <div>
+      <span title='Lowest Possible' style={{ width: `${minScore}%` }} />
+      <span title='Current Score' style={{ width: `${score}%` }} />
+      <span title='Highest Possible' style={{ width: `${maxScore}%` }} />
+    </div>
+  </div>
 }
 
 export default ScoreMeter
