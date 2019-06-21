861dcb4b221e1a570a4d9ab69bdf0e2046cf3d84
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-20 22:46:25 -0400

    move state to reducer, disable score meter

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import 'normalize.css'
 import './Quiz.scss'
 import TopProgressBar from './TopProgressBar'
@@ -9,46 +9,80 @@ import Question from './Question'
 import OptionSet from './OptionSet'
 import Feedback from './Feedback'
 import NextQuestion from './NextQuestion'
-import ScoreMeter from './ScoreMeter'
+// import ScoreMeter from './ScoreMeter'
 import questions from '../questions.json'
 
 function Quiz () {
-  const [ currentRound, setCurrentRound ] = useState(1)
-  const [ mistakes, setMistakes ] = useState(0)
-  const [ answered, setAnswered ] = useState(false)
-  const [ roundCorrect, setRoundCorrect ] = useState(null)
-  const q = questions[currentRound - 1]
+  const [ quiz, dispatch ] = useReducer(function (state, action) {
+    switch (action) {
+      case 'next_question':
+        return {
+          ...state,
+          playerAnswered: false,
+          currentRound: state.currentRound + 1,
+          isAnswerCorrect: null
+        }
+      case 'answer_correct':
+        return {
+          ...state,
+          playerAnswered: true,
+          isAnswerCorrect: true,
+          correctAnswers: state.correctAnswers + 1,
+          score: getPercentage(state.correctAnswers + 1, questions.length)
+        }
+      case 'answer_incorrect':
+        return {
+          ...state,
+          playerAnswered: true,
+          isAnswerCorrect: false,
+          score: getPercentage(state.correctAnswers, state.currentRound)
+        }
+    }
+  }, {
+    currentRound: 1,
+    playerAnswered: false,
+    isAnswerCorrect: null,
+    correctAnswers: 0,
+    score: 100,
+    minScore: 0,
+    maxScore: 100
+  })
 
-  function nextQuestion () {
-    setCurrentRound(currentRound + 1)
-    setAnswered(false)
-    setRoundCorrect(null)
+  const q = questions[quiz.currentRound - 1]
+
+  function getPercentage (val, on) {
+    return Math.floor(val / on * 100)
   }
-  function handleCorrect () {
-    setRoundCorrect(true)
-  }
-  function handleIncorrect () {
-    setMistakes(mistakes + 1)
-    setRoundCorrect(false)
-  }
+  // function nextQuestion () {
+  //   setCurrentRound(currentRound + 1)
+  //   setAnswered(false)
+  //   setRoundCorrect(null)
+  // }
+  // function handleCorrect () {
+  //   setCorrectAnswers(correctAnswers + 1)
+  //   setRoundCorrect(true)
+  // }
+  // function handleIncorrect () {
+  //   setRoundCorrect(false)
+  // }
 
   return <div className='quiz'>
-    <TopProgressBar maxQuestions={questions.length} currentQuestion={currentRound} />
-    <QuestionCounter maxQuestions={questions.length} currentQuestion={currentRound} />
+    <TopProgressBar maxQuestions={questions.length} currentQuestion={quiz.currentRound} />
+    <QuestionCounter maxQuestions={questions.length} currentQuestion={quiz.currentRound} />
     <Category text={q.category} />
     <DifficultyRating difficulty={q.difficulty} />
     <Question text={q.question} />
     <OptionSet
-      answered={answered}
-      setAnswered={setAnswered}
+      answered={quiz.playerAnswered}
+      // setAnswered={setAnswered}
       correctAnswer={q.correct_answer}
       incorrectAnswers={q.incorrect_answers}
-      handleCorrect={handleCorrect}
-      handleIncorrect={handleIncorrect}
+      handleCorrect={() => dispatch('answer_correct')}
+      handleIncorrect={() => dispatch('answer_incorrect')}
       type={q.type} />
-    { roundCorrect !== null && <Feedback correct={roundCorrect} /> }
-    { answered && <NextQuestion handler={nextQuestion} /> }
-    <ScoreMeter maxRounds={questions.length} mistakes={mistakes} currentRound={currentRound} />
+    { quiz.isAnswerCorrect !== null && <Feedback correct={quiz.isAnswerCorrect} /> }
+    { quiz.playerAnswered && <NextQuestion handler={() => dispatch('next_question')} /> }
+    {/* <ScoreMeter maxRounds={questions.length} mistakes={mistakes} currentRound={currentRound} correctAnswers={correctAnswers} /> */}
   </div>
 }
 
